494426d41df47c27ff42ab499baadd3f0c37d7dd
Yaakov Lerer <coby.lerer@outlook.com>
2021-03-12 13:36:05 +1100

    remove comments

--- util/apis/github/checkRun.js
+++ util/apis/github/checkRun.js
@@ -1,18 +1,9 @@
 'use strict';
 
-// const { Octokit } = require("@octokit/rest");
-// const { createAppAuth } = require("@octokit/auth-app");
-
 const githubAPIHandler = require('../../helper/githubAPIHandler');
-
-// const appId = process.env.GITHUB_APP_ID;
-// const installationId = process.env.GITHUB_APP_INSTALL_ID;
-// const begin = '-----BEGIN RSA PRIVATE KEY-----';
-// const end = '-----END RSA PRIVATE KEY-----';
 
 const CHECK_NAME = 'Veracode Upload and Scan';
 const CHECK_RESULT_TITLE = `${CHECK_NAME} Results`;
-//const TEST_RUN_TITLE = 'Veracode Static Scan Results';
 
 const CONCLUSION = {
     SUCCESS : 'success', 
@@ -23,38 +14,6 @@ const CONCLUSION = {
     TIME_OUT: 'timed_out', 
     ACTION_REQUIRED: 'action_required'
 }
-
-// const getAuthApp = async () => {
-//     console.log('getAuthApp - START');
-//     var pem = process.env.PEM;
-
-//     const content = pem.substring(begin.length,pem.length-end.length);
-
-//     const parsedPEM = begin + content.replace(/\s+/gi, "\n") + end;
-//     //console.log(parsedPEM.length);
-  
-//     try {
-//         const appOctokit = new Octokit({
-//             authStrategy: createAppAuth,
-//             auth: {
-//                 appId,
-//                 privateKey: parsedPEM,
-//                 installationId,
-//             },
-//         });
-
-//         //const authRes = 
-//         await appOctokit.auth({
-//             type: "installation",
-//         });
-//         console.log('getAuthApp - END');
-//         return appOctokit;
-//     } catch (e) {
-//         console.log('getAuthApp - END ==   ERROR   ===============================');
-//         console.log(e);
-//     }
-//     return;
-// }
 
 const createCheckRun = async (owner,repo,headSHA) => {
     console.log('createNewCheckRun - START');
@@ -68,7 +27,7 @@ const createCheckRun = async (owner,repo,headSHA) => {
             status: 'queued',
             head_sha: headSHA
         });
-        //console.log(check);
+
     } catch (e) {
         console.log('===========   ERROR   ===============================');
         console.log(e);
@@ -88,7 +47,7 @@ const updateCheckRun = async (owner,repo,checkRunId,input) => {
             repo,
             check_run_id: checkRunId
         });
-        //console.log(check);
+
     } catch (e) {
         console.log('===========   ERROR   ===============================');
         console.log(e);
@@ -96,50 +55,6 @@ const updateCheckRun = async (owner,repo,checkRunId,input) => {
     console.log('updateNewCheckRun - END');
     return check;
 }
-
-// const closeCheckRun = async (appGUID,sandboxGUID, buildId, checkRunId) => {
-//     const summary = await buildSummaryHandler.getBuildSummary(appGUID,sandboxGUID,buildId);
-//     //console.log(summary);
-//     const summaryMD = buildSummaryHandler.getBuildSummaryMarkDown(summary);
-//     const textMD = buildSummaryHandler.getBuildSumaryDetails(summary);
-//     console.log(textMD);
-//     console.log(summaryMD);
-//     const updateCheckResponse = await checkRunHandler.updateCheckRun('Lerer','veracode-async',checkRunId,{
-//         status: 'completed',
-//         // todo - update conclusion
-//         conclusion: 'neutral',
-//         output: {
-//             summary: summaryMD,
-//             title: CHECK_RESULT_TITLE,
-//             text: textMD
-//         }
-//     });
-//     return updateCheckResponse;
-// };
-
-// const listRepoIssue = async () => {
-//     console.log('listRepoIssue - START');
-   
-//     try {
-//         const appOctokit = githubAPIHandler.getAuthApp();
-    
-//         const res = await appOctokit
-//         //.request('GET /repos/{owner}/{repo}/issues/{issue_number}', {
-//             .issues.listForRepo({
-//             owner: 'Lerer',
-//             repo: 'veracode-async',
-//             //issue_number: 5,
-//             //body: 'Issue comment',
-//             //title: 'Issue title'
-//         });
-  
-//         console.log(res);
-//     } catch (e) {
-//         console.log('=======================   ERROR   ===============================');
-//         console.log(e);
-//     }
-//     console.log('listRepoIssue - END');
-// };
 
 const baseSQSMessageFromGithubEvent = (eventJson) => {
     if (typeof eventJson === 'string'){
@@ -164,11 +79,9 @@ const baseSQSMessageFromGithubEvent = (eventJson) => {
 }
 
 module.exports = {
-    //listRepoIssue,
     createCheckRun,
     baseSQSMessageFromGithubEvent,
     updateCheckRun,
-    // closeCheckRun,
     CHECK_RESULT_TITLE,
     CONCLUSION
 }
